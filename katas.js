b45d2ee449322a8b4b9a99d16c2acd18459a9d28
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-03-27 15:34:36 +0100

    vowels

--- katas.js
+++ katas.js
@@ -289,3 +289,24 @@ function pyramid(n) {
 }
 
 
+
+//VOWELS
+/*--- Directions
+Write a function that returns the number of vowels
+used in a string.  Vowels are the characters 'a', 'e'
+'i', 'o', and 'u'.
+--- Examples
+  vowels('Hi There!') --> 3
+  vowels('Why do you ask?') --> 4
+  vowels('Why?') --> 0 */
+
+function vowels(str) {
+  let counter = 0;
+  for (let char of str.toLowerCase()) {
+    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+      counter++
+    }
+  }
+  return counter
+}
+
