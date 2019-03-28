dd083063d9f6853ebeefb66d8f9b52664eff247e
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-03-28 15:54:32 +0100

    FIB

--- katas.js
+++ katas.js
@@ -373,3 +373,29 @@ function matrix(n) {
 
 
 }
+
+
+
+//FIB
+/*--- Directions
+Print out the n-th entry in the fibonacci series.
+The fibonacci series is an ordering of numbers where
+each number is the sum of the preceeding two.
+For example, the sequence
+ [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+forms the first ten entries of the fibonacci series.
+Example:
+  fib(4) === 3*/
+
+function fib(n) {
+  let result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1]
+    const b = result[i - 2]
+
+    result.push(a + b)
+  }
+  return result[result.length - 1]
+}
+
